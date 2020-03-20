cfaf0f956eb4e803de5c68c1a149adf47b524fe1
Andrejs Zmakins <andrejs.zmakins@accenture.com>
2020-03-20 18:42:05 +0200

    Better unit test coverage for AirportController. Some code improvements.

---
+++ src/test/java/flights/flighttracker/AirportControllerEndpointTest.java
@@ -0,0 +1,66 @@
+package flights.flighttracker;
+
+import flights.flighttracker.airport.AirportController;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Test for <code>AirportController</code>.
+ *
+ * Numbers in method names are necessary to enforce test order and save some time on mock mvc instantiation.
+ * @see AirportController
+ * @author Andrejs Zmakins
+ */
+public class AirportControllerEndpointTest extends FlightTrackerApplicationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    @Test
+    public void test1GetAllAirports() throws Exception {
+        mockMvc.perform( get("/airports") )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"));
+
+    }
+
+
+    @Test
+    public void test2GetAirportById() throws Exception {
+        mockMvc.perform( get("/airports/1") )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
+
+
+    @Test
+    public void test3DeleteAirportPathParam() throws Exception {
+        mockMvc.perform( delete("/airports/1") )
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void test4DeleteAirportQueryParam() throws Exception {
+        mockMvc.perform( delete("/airports/?id=2") )
+                .andExpect(status().isOk());
+    }
+}
